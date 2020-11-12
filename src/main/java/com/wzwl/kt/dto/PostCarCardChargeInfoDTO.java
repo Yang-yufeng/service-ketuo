7cd4938c7452d01501c1c9130ef520b41d8b6c65
Yang-yufeng <542796637@qq.com>
2020-11-12 18:21:29 +0800

    修改

---
+++ src/main/java/com/wzwl/kt/dto/PostCarCardChargeInfoDTO.java
@@ -0,0 +1,13 @@
+package com.wzwl.kt.dto;
+
+import com.wzwl.kt.dto.BaseDTO;
+import lombok.Data;
+
+/**
+ * @author huff
+ * @date 2020/11/12 17:28
+ */
+public class PostCarCardChargeInfoDTO extends BaseDTO {
+
+
+}
